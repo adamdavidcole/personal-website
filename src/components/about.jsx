9bf8d82e7ba237adb42c18cae02968864ec85fb9
Adam Cole <acole@slack-corp.com>
2020-11-22 03:34:08 -0800

    slack engineering images and description placeholder

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -4,9 +4,10 @@ import Footer from "./footer";
 
 export default function About() {
   return (
-    <>
+    <div style={{ margin: "30px" }}>
       <h1>About</h1>
+      <p>Coming soon!...</p>
       <Footer />
-    </>
+    </div>
   );
 }
--- src/components/resume.jsx
+++ src/components/resume.jsx
@@ -6,6 +6,7 @@ export default function Home() {
   return (
     <>
       <div className="p-resume__file_container">
+        <h2>Resume</h2>
         <iframe
           src={process.env.PUBLIC_URL + "AdamCole - CV.pdf"}
           width="100%"
