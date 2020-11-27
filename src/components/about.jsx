390338e11fa4ef1b39a066d79e712626402ce35d
Adam Cole <acole@slack-corp.com>
2020-11-27 03:50:48 -0800

    add about me page content

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -4,10 +4,57 @@ import Footer from "./footer";
 
 export default function About() {
   return (
-    <div style={{ margin: "30px" }}>
-      <h1>About</h1>
-      <p>Coming soon!...</p>
-      <Footer />
+    <div className="p-about">
+      <div className="p-about__content">
+        <div className="p-about__title">
+          <h2>About Me</h2>
+        </div>
+
+        <div className="p-about__description">
+          <p>👋 Hi there,</p>
+          <h3 className="p-about__description_headline">
+            I'm a software engineer by day, a film junkie by night, and am
+            personally interested in the creative technologies that can bridge
+            the gap between those two worlds.
+          </h3>
+
+          <p>
+            My interests in tech and art have always been intertwined. This was
+            especially true when I attended the University of Pennsylvania in
+            2012 where I was lucky to be part of small degree program called
+            Digital Media Design, an interdisciplinary major that combined
+            courses in computer graphics, media theory, and fine arts.
+          </p>
+
+          <p>
+            After graduation, I hopped over to San Francisco to join what was
+            then a relatively small company called Slack as a frontend engineer.
+            I joined the company because of its reputation for prioritizing user
+            experience design, the centrality of its frontend team to the
+            product, and the general whimsy sprinkled throughout the app. Having
+            been on the frontend messaging team for the past four years, I see
+            my mission there as maintaining that attention to detail and
+            excellent product experience for the individual user while
+            simultaneously designing a system that can scale to millions of
+            different people, both from a technical and product perspective.
+          </p>
+
+          <p>
+            Having focused on my client engineering and interaction design
+            skills for the past few years, I am looking for new opportunities to
+            return to my creative roots, especially when it comes to animation
+            and film. I think modern technologies, whether it be VR/AR or the
+            like, will have a huge impact on the language of cinema and I want
+            to be a part of the next generation that redefines the medium.
+          </p>
+        </div>
+        <div className="p-about__heashot_containter">
+          <img
+            src={process.env.PUBLIC_URL + "/img/about-me/headshot1.jpg"}
+            alt=""
+          />
+        </div>
+      </div>
     </div>
   );
 }
